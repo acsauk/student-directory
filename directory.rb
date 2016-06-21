42481706f906300c928552761c9f0344f1d0c9a7
acsauk <acsauk@gmail.com>
2016-06-21 16:07:07 +0100

    Update to prtin method to only print names beginning with specific character

--- directory.rb
+++ directory.rb
@@ -25,7 +25,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+     if students[index][:name].start_with?("A")
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
