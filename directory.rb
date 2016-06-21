be83afb253e49ee7f51c4c81625c4035e0549652
acsauk <acsauk@gmail.com>
2016-06-21 16:09:29 +0100

    print method only prints names less than 12 chars

--- directory.rb
+++ directory.rb
@@ -25,7 +25,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-     if students[index][:name].start_with?("A")
+     if students[index][:name].length < 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
