b221684414128c5d87299cc8d0861169afb2cfca
acsauk <acsauk@gmail.com>
2016-06-23 11:50:24 +0100

    Added interactive_menu method to prompt for actions from user

--- directory.rb
+++ directory.rb
@@ -14,11 +14,15 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 =end
+
+
 def input_students
   puts "Please enter the name of students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
+
   # get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
@@ -48,10 +52,33 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def interactive_menu
+  # create an empty array
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1 Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 # Nothing happens until we call the methods, feeding
 # in the required parameters to the arguments
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
